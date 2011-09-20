b7bcfe42a7444b9c71c3c11cdc51e5ee27f921ca
DBJDBJ <dbjdbj@gmail.com>
2011-09-21 00:32:34 +0100

    dramatic improvements error script loading event caught first argument as final call-back made optional

--- dbj.loader.js
+++ dbj.loader.js
@@ -8,16 +8,17 @@
 	, "http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
 	);
 	
-	First argument must be final calback or null. If null then default callback is used which shows
-	alert with source urls of all scrpts dbjLOAD-ed
+	If first argument is a function it is going to be used as final calback. 
 	
  * Copyright (c) 2011 DBJ.ORG
  * Dual licensed under the MIT or GPL licenses.
  */
 
 (function (global, undefined) {
-	var scripts_loaded = [],
-	addprops = function (t) {
+	global.console || (global.console = {
+		log: function () { }, error: function () { }, warn: function () { }
+	});
+	var addprops = function (t) {
 		var j = 1; while (arguments[j]) { t[arguments[j++]] = arguments[j++]; } return t;
 	},
 	next_script = function (src, next_cb_) {
@@ -26,22 +27,41 @@
 				"type", "text/javascript",
 				"id", escape(src),
 				"src", src,
-				"onload", next_cb_)
+				"onload", function (e) {
+					e || (e = window.event);
+					global.__dbj__loader__.scripts_loaded.push((e.target || e.srcElement).src);
+					next_cb_();
+					return false;
+				},
+				"onerror", function (e) {
+					e || (e = window.event);
+					global.__dbj__loader__.scripts_errored.push((e.target || e.srcElement).src);
+					next_cb_();
+					return false;
+				}
+			)
 		);
-		scripts_loaded.push(src);
-	},
-	default_finale = function () { alert(scripts_loaded.join("\n")); };
+	};
 
 	global.dbjLOADER = function () {
-		var args = Array.prototype.slice.call(arguments), arlen = args.length >>> 0, args_idx = 0;
+		var args = Array.prototype.slice.call(arguments), arlen = args.length >>> 0, args_idx = 0,
+		user_finale = "function" === typeof args[0] ? args[0] : false ,
+		default_finale = function () {
+			console.log(global.__dbj__loader__.scripts_loaded.join("\n"));
+			console.error(global.__dbj__loader__.scripts_errored.join("\n"));
+			if (user_finale) user_finale();
+		};
 		args.push("dummy.js");
 		global.__dbj__loader__ = function () {
 			next_script(
 					args[args_idx],
-					(args_idx += 1) < arlen ? __dbj__loader__ : args[0] || default_finale
+					(args_idx += 1) < arlen ? __dbj__loader__ : default_finale
 				);
-		};
-		__dbj__loader__(args_idx = 1);
+		}
+		global.__dbj__loader__.scripts_loaded = ["dbjLOADER :: loaded scripts"],
+			global.__dbj__loader__.scripts_errored = ["dbjLOADER :: errored scripts"];
+
+		__dbj__loader__( args_idx = !! user_finale ? 1 : 0 );
 	};
 
 } (window));

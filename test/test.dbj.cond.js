59b51e117c0157360115394e8c0966c7e620c86e
DBJDBJ <dbjdbj@gmail.com>
2013-07-24 00:58:03 +0100

    still a puzzle

--- test/test.dbj.cond.js
+++ test/test.dbj.cond.js
@@ -22,11 +22,31 @@
             + JSON.stringify(x) + ", and " + JSON.stringify(v));
 
         testera('dbj.cond(test.x, test.v, "x eq v", "!")', "!");
-        testera('dbj.cond(test.x, 0, "1", 2, 3, 4, 5, 6, 7, "!")', "!");
         testera('dbj.cond(true, test.x != test.v, "neq","!")', "neq");
         testera('dbj.cond(true, 1 == 2, "1", test.v)', test.v);
         testera('dbj.cond(true, 1 == 1, test.x, "!")', test.x);
     }
+    /*
+    precondition: x === v
+    */
+        function test_for_equality(x) {
+
+            test.x = test.v = x;
+
+            ok(test.x === test.v, "-------------------------------------------------------------| Test with: "
+                + JSON.stringify(test.x) + ", and " + JSON.stringify(test.v));
+
+            testera('dbj.cond(test.x, test.v, "EQ", "!")', "EQ");
+            testera('dbj.cond(true, test.x != test.v, "neq","!")', "!");
+            testera('dbj.cond(true, 1 == 2, "NEQ", test.v)', test.v);
+            testera('dbj.cond(true, 1 == 1, test.x, "!")', test.x);
+        }
+
+        function multi_test(x, v)
+        {
+            test_for_not_equality(x, v);
+            test_for_equality(x);
+        }
 
     test(" presence of the library ", function () {
         ok(!!dbj, "dbj is defined");
@@ -35,10 +55,10 @@
 
     test(" dbj.cond.comparator => standard ", function () {
         /* standard is used by default */
-        test_for_not_equality(1, 2);
-        test_for_not_equality("Alpha", "Beta");
-        test_for_not_equality(true, false);
-        test_for_not_equality(3.14, 2.34);
+        multi_test(1, 2);
+        multi_test("Alpha", "Beta");
+        multi_test(true, false);
+        multi_test(3.14, 2.34);
 
     });
 
@@ -46,10 +66,10 @@
 
     test(" dbj.cond.comparator => dbj.compare.deep ", function () {
 
-        test_for_not_equality({}, {1:2});
-        test_for_not_equality({ "Alpha": 1 }, { "Beta": 2 } );
-        test_for_not_equality([true,true], [false,false]);
-        test_for_not_equality([3, 2], [2, 3]);
+        multi_test({}, { 1: 2 });
+        multi_test({ "Alpha": 1 }, { "Beta": 2 });
+        multi_test([true, true], [false, false]);
+        multi_test([3, 2], [2, 3]);
 
     });
 
@@ -57,10 +77,10 @@
 
     test(" dbj.cond.comparator => dbj.compare.multi ", function () {
 
-        test_for_not_equality({}, [{ 1: 2 }]);
-        test_for_not_equality([{ "Alpha": 1 }], { "Beta": 2 });
-        test_for_not_equality([[true, true],3], [false, false]);
-        test_for_not_equality([3, 2], [true,[3, 2]]);
+        multi_test({}, [{ 1: 2 }]);
+        multi_test([{ "Alpha": 1 }], { "Beta": 2 });
+        multi_test([[true, true], 3], [false, false]);
+        multi_test([3, 2], [true, [3, 2]]);
 
     });
 

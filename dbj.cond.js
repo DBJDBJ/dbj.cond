f62e88cedefec0b12f0b2e9d25ab2c902b42234a
dbjdbj <dbj@dbj.org>
2018-01-17 23:06:16 +0100

    switched to Appache 2.0 license

--- dbj.cond.js
+++ dbj.cond.js
@@ -1,9 +1,18 @@
+
 /*
-(c) 2011 - 2018 by DBJ.ORG
- Dual licensed under the MIT (MIT-LICENSE.txt)
- and GPL (GPL-LICENSE.txt) licenses.
+Copyright 2018 dbj.org
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
 */
-(function (dbj,undefined) {
+
+(function (dbj, undefined) {
 
     var /* coercion to Int32 as required by asm.js */
         toInt32 = function (v_) {

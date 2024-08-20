6b45f49e9f8d200d19ce1934f53b48200947261c
dbj <dbj@dbj.org>
2024-08-20 20:55:55 +0200

    Update dbj.cond.js

--- dbj.cond.js
+++ dbj.cond.js
@@ -1,6 +1,6 @@
 
 /*
-Copyright 2018 dbj.org
+Copyright 2018 - 2024 by dbj.org
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
